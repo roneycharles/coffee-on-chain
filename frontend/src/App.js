065cdab78dbdddc878901f1f364dcf2b06a318a6
Fernando Sobreira <fbsobreira@gmail.com>
2020-01-30 16:53:30 -0300

    Add tronweb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import Button from './button';
 import Display from './display';
+import { buttons } from './constants'
+
+//const TronWeb = require('tronweb')
+
+const contractAddress = "TAizHsUbdDK7XL2QUqdjLxNkvgxNZB5Qbs"
+const machineID = "0x0b30a05d1748322d148bc8000bdf22c077759ad42ad3766b27a99830a200f97d"
 
 class App extends React.Component {
 
@@ -12,94 +18,106 @@ class App extends React.Component {
       height: window.innerHeight,
       ratio: (1316 / 1200),
       btnClick: -1,
-      textLed: ''
+      textLed: '',
+      loading: false,
+      tronWeb: {},
+      myAddress: "",
+      contract: null,
+      products: []
     }
+    
     window.addEventListener("resize", this.updateWidthAndHeight);
-    this.btn1 = {
-      left: 462,
-      top: 234
-    }
-
-    this.buttons = [{
-      id: 1,
-      top: 19,
-      left: 27,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 2,
-      top: 36,
-      left: 27,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 3,
-      top: 52.7,
-      left: 27,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 4,
-      top: 69.5,
-      left: 27,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 5,
-      top: 19,
-      left: 52,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 6,
-      top: 36,
-      left: 52,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 7,
-      top: 52.7,
-      left: 52,
-      width: 21,
-      height: 14.5
-    },
-    {
-      id: 8,
-      top: 69.5,
-      left: 52,
-      width: 21,
-      height: 14.5,
-    },
-    {
-      id: 9,
-      top: 32,
-      left: 82,
-      width: 14,
-      height: 8,
-      component: (<><br></br><li>Confirmar</li></>)
-    }
-    ];
   }
 
 
   componentDidMount() {
     this.updateWidthAndHeight()
+    
+    this.setState({
+      loading:true
+    })
+
+    this.loadPrices = setInterval( async () => {
+      const { tronWeb, contract } = this.state;
+      if (!tronWeb.loggedIn || contract == null) return;
+
+      const products = [];
+      for (let i=0; i< 8; i++){
+        const ret = await contract.getProductPriceAndName(machineID, i).call();
+        products.push({
+          index: i,
+          name: ret.name,
+          price: ret.price.toString()
+        })
+      }
+      this.setState({
+        products
+      });
+    }, 5000);
+
+    const loadWatcher = setInterval( async () => {
+      if(window.tronWeb && window.tronWeb.ready) {            
+          const installed = !!window.tronWeb;
+          const tronWebState = {
+              installed: installed,
+              loggedIn: true,
+          };
+          const contract = await window.tronWeb.contract().at(contractAddress);
+          this.setState({
+              tronWeb: tronWebState,
+              loading: false,
+              myAddress: window.tronWeb.defaultAddress.base58,
+              contract
+          });
+          clearInterval(loadWatcher);
+      }
+      if(this.state.tronWeb.loggedIn) {
+          window.tronWeb.on('addressChanged', (address) => {
+              console.log("Changed address", address)
+              if(this.state.tronWeb.loggedIn){
+                this.setState({
+                    myAddress: address.base58
+                });
+                return;
+              }
+                
+
+              this.setState({
+                  tronWeb: {
+                      ...this.state.tronweb,
+                      installed: true,
+                      loggedIn: true,
+                  },
+                  myAddress: address.base58
+              });
+          });
+      }
+    }, 500)
   }
+  
+  handleClickButton = async ({ id }) => {
 
-  componentDidUpdate() {
+    const { btnClick, contract } = this.state;
+    if (id===9) {
+      const { products } = this.state;
+      if (btnClick<0) {
+        alert("Select first")
+        return
+      }
+      if (btnClick >= products.length) {
+        alert("Invalid selection")
+        return
+      }
+      const ret = await contract.pay(machineID, btnClick).send({
+        callValue: products[btnClick].price,
+        shouldPollResponse: true
+      })
+      console.log("Paid", ret)
 
-  }
 
-  handleClickButton({ id }) {
-
-    const { btnClick } = this.state;
-
+      
+      return
+    }
     if (btnClick === id) {
       return this.setState({ btnClick: -1 })
     }
@@ -123,28 +141,35 @@ class App extends React.Component {
   };
 
   render() {
-    const { width, height, btnClick } = this.state;
+    const { width, height, btnClick, myAddress, products } = this.state;
+    
+    const selectedPrice = (btnClick>0 && btnClick<products.length)?products[btnClick].price:-1
 
     return (
       <div className="App">
         <div className="app-background" style={{ width, height }}>
-          {this.buttons.map(button => (
-            <Button onClick={() => this.handleClickButton(button)} top={button.top} left={button.left} width={button.width} height={button.height} active={btnClick == button.id ? true : false} component={button.component} />
+          {buttons.map(button => (
+            <Button key={button.id} onClick={() => this.handleClickButton(button)} top={button.top} left={button.left} width={button.width} height={button.height} active={btnClick === button.id ? true : false} component={button.component} />
           ))}
           <div className="app-display">
-            <Display top={3.5} left={41.2} width={17.8} height={8.9} active={true} component={"CoffeeOnChain"} />
+            <Display top={3.5} left={41.2} width={17.8} height={8.9} active={true} component={
+              <div>
+                <div>
+                CoffeeOnChain
+                </div>
+                <div>
+                {myAddress}
+                </div>
+                {
+                  (selectedPrice>=0) &&
+                  <div>
+                    Valor: R${selectedPrice}
+                  </div>
+                }
+              </div>
+            } />
 
           </div>
-
-          {/* 
-          <Button onClick={() => this.setState({ btnClick: 1 })} top={19} left={27} width={21} height={14.5} active={this.state.btnClick === 1 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 2 })} top={36} left={27} width={21} height={14.5} active={this.state.btnClick === 2 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 3 })} top={52.7} left={27} width={21} height={14.5} active={this.state.btnClick === 3 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 4 })} top={69.5} left={27} width={21} height={14.5} active={this.state.btnClick === 4 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 5 })} top={19} left={52} width={21} height={14.5} active={this.state.btnClick === 5 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 6 })} top={36} left={52} width={21} height={14.5} active={this.state.btnClick === 6 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 7 })} top={52.7} left={52} width={21} height={14.5} active={this.state.btnClick === 7 ? true : false} />
-          <Button onClick={() => this.setState({ btnClick: 8 })} top={69.5} left={52} width={21} height={14.5} active={this.state.btnClick === 8 ? true : false} /> */}
         </div>
 
 
---
+++ frontend/src/constants.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export const buttons = [{
+      id: 1,
+      top: 19,
+      left: 27,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 2,
+      top: 36,
+      left: 27,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 3,
+      top: 52.7,
+      left: 27,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 4,
+      top: 69.5,
+      left: 27,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 5,
+      top: 19,
+      left: 52,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 6,
+      top: 36,
+      left: 52,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 7,
+      top: 52.7,
+      left: 52,
+      width: 21,
+      height: 14.5
+    },
+    {
+      id: 8,
+      top: 69.5,
+      left: 52,
+      width: 21,
+      height: 14.5,
+    },
+    {
+      id: 9,
+      top: 32,
+      left: 82,
+      width: 14,
+      height: 8,
+      component: (<div className="app-confirmation">
+          <p>Confirmar</p>
+        </div>)
+    }
+    ];
